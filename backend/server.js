d951d298a0232e39bee25e7093114e029454db55
Priyani Gulgulia <jainpriyani272@gmail.com>
2026-08-21 20:50:24 +0530

    Configure backend for Render

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
